ce2c7acc5cd67d0b4ebd2c1f19e1a58378012004
ABugNamedBeetle <sharma18arpit@gmail.com>
2025-12-20 19:00:45 +0530

    updated health api

---
+++ app/sbs.bank/src/main/java/io/sbs/bank/controller/UtilityController.java
@@ -0,0 +1,20 @@
+package io.sbs.bank.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class UtilityController {
+    @GetMapping("health")
+    public Map<String, String> getHealth(@RequestParam String param) {
+        return Map.of("status", "OK", "time", LocalDateTime.now().toString());
+    }
+    
+    
+}
